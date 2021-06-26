d7173d08bd333b75dfe19c9ed2603c169f02bc74
Daisy <misaki_yachu@yahoo.com.tw>
2021-06-26 17:28:30 +0800

    init

---
+++ src/components/NewestComments.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+      最新評論
+    </div>
+    <div class="card-body">
+      <div
+        v-for="comment in comments"
+        :key="comment.id"
+      >
+        <h4>
+          <a href="#">
+            {{ comment.Restaurant.name }}
+          </a>
+        </h4>
+        <p>{{ comment.text }}</p>by
+        <a href="#">
+          {{ comment.User.name }}
+        </a>
+        {{ comment.createdAt | fromNow }}
+        <hr>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { fromNowFilter } from './../utils/mixins'
+
+export default {
+  mixins: [fromNowFilter],
+}
+</script>
