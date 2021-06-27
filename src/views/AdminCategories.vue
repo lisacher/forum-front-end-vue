308395ba3960089ee207bfd22b48e94e6af5bdde
Daisy <misaki_yachu@yahoo.com.tw>
2021-06-27 22:31:23 +0800

    add admin users

--- src/views/AdminCategories.vue
+++ src/views/AdminCategories.vue
@@ -46,32 +46,59 @@
       </thead>
       <tbody>
         <tr
-          v-for="category in categories"
-          :key="category.id"
-        >
-          <th scope="row">
-            {{ category.id }}
-          </th>
-          <td class="position-relative">
-            <div class="category-name">
-              {{ category.name }}
-            </div>
-          </td>
-          <td class="d-flex justify-content-between">
-            <button
-              type="button"
-              class="btn btn-link mr-2"
-            >
-              Edit
-            </button>
-            <button
-              type="button"
-              @click.stop.prevent="deleteCategory(category.id)"
-              class="btn btn-link mr-2"
-            >
-              Delete
-            </button>
-          </td>
+      v-for="category in categories"
+      :key="category.id"
+    >
+     <th scope="row">
+      {{ category.id }}
+     </th>
+        <td class="position-relative">
+          <div
+            v-show="!category.isEditing"
+            class="category-name"
+          >
+            {{ category.name }}
+          </div>
+          <input
+            v-show="category.isEditing"
+            v-model="category.name"
+            type="text"
+            class="form-control"
+          >
+          <span
+            v-show="category.isEditing"
+            @click="handleCancel(category.id)"
+            class="cancel"
+          >
+            ✕
+          </span>
+        </td>
+        <td class="d-flex justify-content-between">
+          <button
+            v-show="!category.isEditing"
+            @click.prevent.stop="toggleIsEditing(category.id)"
+            type="button"
+            class="btn btn-link mr-2"
+          >
+            Edit
+          </button>
+          <button
+            v-show="category.isEditing"
+            type="button"
+            @click.stop.prevent="
+            updateCategory({ categoryId: category.id, name: category.name })"
+            class="btn btn-link mr-2"
+          >
+            Save
+          </button>
+          <button
+            type="button"
+            class="btn btn-link mr-2"
+            @click.stop.prevent="deleteCategory(category.id)"
+          >
+            Delete
+          </button>
+        </td>
         </tr>
       </tbody>
     </table>
@@ -79,6 +106,7 @@
 </template>
 
 <script>
+/* eslint-disable */
 import {v4 as uuidv4} from 'uuid'
 import AdminNav from '@/components/AdminNav'
 //  2. 定義暫時使用的資料
@@ -130,7 +158,11 @@ export default {
   methods: {
     // 4. 定義 `fetchCategories` 方法，把 `dummyData` 帶入 Vue 物件
     fetchCategories () {
-      this.categories = dummyData.categories
+      this.categories = dummyData.categories.map(category => ({
+        ...category,
+        isEditing: false,
+        nameCached: ''
+      }))
     },
     createCategory () {
       // TODO: 透過 API 告知伺服器欲新增的餐廳類別...
@@ -150,7 +182,68 @@ export default {
       this.categories = this.categories.filter(
         category => category.id !== categoryId
       )
+    },
+    toggleIsEditing (categoryId) {
+    this.categories = this.categories.map(category => {
+        if (category.id === categoryId) {
+        return {
+            ...category,
+            isEditing: !category.isEditing,
+            nameCached: category.name
+        }
+        }
+
+        return category
+    })
+    },
+    updateCategory ({ categoryId, name }) {
+      // TODO: 透過 API 去向伺服器更新餐廳類別名稱
+      this.toggleIsEditing(categoryId)
+    },
+    handleCancel (categoryId) {
+    this.categories = this.categories.map(category => {
+        if (category.id === categoryId) {
+        return {
+            ...category,
+
+            // 把原本的餐廳類別名稱還回去
+            name: category.nameCached
+        }
+    }
+    return category
+    })
+    this.toggleIsEditing(categoryId)
     }
   }
 }
 </script>
+
+<style scoped>
+.category-name {
+  padding: 0.375rem 0.75rem;
+  border: 1px solid transparent;
+  outline: 0;
+  cursor: auto;
+}
+
+.btn-link {
+  width: 62px;
+}
+
+.cancel {
+  position: absolute;
+  right: 20px;
+  top: 50%;
+  transform: translateY(-50%);
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  width: 25px;
+  height: 25px;
+  border: 1px solid #aaaaaa;
+  border-radius: 50%;
+  user-select: none;
+  cursor: pointer;
+  font-size: 12px;
+}
+</style>
